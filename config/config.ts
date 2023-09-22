58e6cb79d97afc86647501ff15960fa38b00497e
Qingchenaini <2822948620@qq.com>
2023-09-22 09:48:15 +0800

    fix: 替换标题

--- config/config.ts
+++ config/config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'Ant Design Pro',
+  title: '猴智能BI',
   layout: {
     locale: true,
     ...defaultSettings,
@@ -91,10 +91,10 @@ export default defineConfig({
    * @name 国际化插件
    * @doc https://umijs.org/docs/max/i18n
    */ /**
-   * @name antd 插件
-   * @description 内置了 babel import 插件
-   * @doc https://umijs.org/docs/max/antd#antd
-   */
+  * @name antd 插件
+  * @description 内置了 babel import 插件
+  * @doc https://umijs.org/docs/max/antd#antd
+  */
   antd: {},
   /**
    * @name 网络请求配置
@@ -131,7 +131,7 @@ export default defineConfig({
       requestLibPath: "import { request } from '@umijs/max'",
       // 或者使用在线的版本
       schemaPath: "http://localhost:8101/api/v2/api-docs",
-      projectName:"yubi",
+      projectName: "yubi",
       // schemaPath: join(__dirname, 'oneapi.json'),
       mock: false,
     },
--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -152,8 +152,8 @@ const Login: React.FC = () => {
             maxWidth: '75vw',
           }}
           logo={<img alt="logo" src="/logo.svg" />}
-          title="Ant Design"
-          subTitle={'Ant Design 是西湖区最具影响力的 Web 设计规范'}
+          title="猴智能BI"
+          subTitle={'猴智能BI 是西湖区最具影响力的 Web 设计规范'}
           initialValues={{
             autoLogin: true,
           }}
